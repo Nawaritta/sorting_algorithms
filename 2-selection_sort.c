0a01a27b673057e3f922e4ac92bea330ab5dff38
Nawaritta <Nor.benhaddou@gmail.com>
2023-07-22 02:54:37 +0000

    mandatory tasks

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+/**
+ *selection_sort - sorts an array of integers in ascending order
+                   using selection sorting algorithm
+ * @array: the array of integers to sort
+ * @size: the array size
+ */
+void selection_sort(int *array, size_t size)
+{
+
+	size_t i, j, min_index;
+	int tmp;
+
+	for (i = 0; i < size; i++)
+	{
+		min_index = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
+		tmp = array[i];
+		array[i] = array[min_index];
+		array[min_index] = tmp;
+		print_array(array, size);
+	}
+}
---
+++ 3-quick_sort.c
@@ -0,0 +1,54 @@
+#include "sort.h"
+
+/**
+ * quick_sort_rec - recursive function to sort elements
+                using the quick sorting algorithm.
+ * @array: pointer to the array to sort.
+ * @size: the size of the array to sort.
+ * @partition: pointer to the partition to sort.
+ * @part_size: the size of the partition to sort.
+ */
+
+void quick_sort_rec(int *array, size_t size, int *partition, size_t part_size)
+{
+	int pivot, tmp;
+	size_t current = 0, cursor = 0;
+
+	if (partition == NULL || part_size < 2)
+		return;
+
+	pivot = partition[part_size - 1];
+
+	for (; current < part_size; current++)
+	{
+		if (partition[current] <= pivot)
+		{
+			if (current == cursor)
+			{
+				cursor++;
+				continue;
+			}
+			tmp = partition[cursor];
+			partition[cursor++] = partition[current];
+			partition[current] = tmp;
+		      	print_array(array, size);
+		}
+	}
+
+	quick_sort_rec(array, size, partition, cursor - 1);
+	partition += cursor;
+	part_size -= cursor;
+      	quick_sort_rec(array, size, partition, part_size);
+}
+
+/**
+ * quick_sort - sorts an array of integers in ascending order
+                using the quick sorting algorithm.
+ * @array: The array to sort.
+ * @size: the size of the array to sort.
+ */
+
+void quick_sort(int *array, size_t size)
+{
+       	quick_sort_rec(array, size, array, size);
+}
