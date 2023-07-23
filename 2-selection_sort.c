f28a90a387816ac540a2279e2baf950548bb3f9a
Nawaritta <Nor.benhaddou@gmail.com>
2023-07-23 16:02:14 +0000

    mandatory tasks should be fixed

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,7 +9,7 @@ void selection_sort(int *array, size_t size)
 {
 
 	size_t i, j, min_index;
-	int tmp;
+	int tmp, swap = 0;
 
 	if (array == NULL || size < 2)
 		return;
@@ -20,11 +20,18 @@ void selection_sort(int *array, size_t size)
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_index])
+			{
+				swap = 1;
 				min_index = j;
+			}
 		}
-		tmp = array[i];
-		array[i] = array[min_index];
-		array[min_index] = tmp;
-		print_array(array, size);
+		if (swap)
+		{
+			tmp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = tmp;
+			print_array(array, size);
+			swap = 0;
+		}
 	}
 }
